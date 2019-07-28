757866bfcc394abc336ba4d212249331f3b71f79
pscompsci <pstacey@deakin.edu.au>
2019-07-28 20:35:45 +1000

    Additional fields in server struct

--- postgres/up.sql
+++ postgres/up.sql
@@ -4,10 +4,25 @@ CREATE TABLE servers
 (
     server_id uuid DEFAULT uuid_generate_v4 (),
     server_name TEXT NOT NULL,
+    cpu INTEGER,
+    memory INTEGER,
+    storage NUMERIC,
     PRIMARY KEY (server_id)
 );
 
-INSERT INTO servers (server_name) VALUES ('CERTTR-PRD-01');
-INSERT INTO servers (server_name) VALUES ('CARDAX-PRD_01');
-INSERT INTO servers (server_name) VALUES ('SAPGDB-PRD-01');
-INSERT INTO servers (server_name) VALUES ('TABLEU-PRD-01');
+INSERT INTO servers
+    (server_name)
+VALUES
+    ('CERTTR-PRD-01');
+INSERT INTO servers
+    (server_name)
+VALUES
+    ('CARDAX-PRD_01');
+INSERT INTO servers
+    (server_name)
+VALUES
+    ('SAPGDB-PRD-01');
+INSERT INTO servers
+    (server_name)
+VALUES
+    ('TABLEU-PRD-01');
